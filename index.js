8e2479af51c5b10e78ca34e6d32a7d86e189d2d1
Ankitchaudhary2003 <ac3298883@gmail.com>
2024-11-26 23:14:12 -0500

    first

---
+++ index.js
@@ -0,0 +1,57 @@
+// // 1.Declare variable in javaScript
+/* let a=10
+   var b=20
+   const c=30*/
+
+ //print values
+/* console.log(a)
+   console.log(b)
+   console.log(c)*/
+
+// 2. Data types in JavaScript
+/* (i)Primitive-> Number,boolean,String,null,undefined,symbol,bigint.
+   (ii)Non-Primitive-> Array,Object,Function.*/
+
+
+
+// Bigint------>>>>> to store this number we type 192839283723434534n
+/*let account_number=192839283723434534
+console.log(account_number)*/
+
+
+//Types---->
+/*let account_number=10234
+let a=true
+let name="Ankit"
+let b=null
+let c
+console.log(typeof(account_number))    //number
+console.log(typeof(a))             //boolean
+console.log(typeof(name))           //string
+console.log(typeof(b))            //object
+console.log(typeof(c))            //undefined   */
+
+//Non-primitive datatypes
+
+/*let arr=[10,20,"Ankit",'2.O',null]
+console.log(arr)
+console.log(typeof(arr))      //object       
+
+let obj={
+    name:"Ankit",
+    fathers_name:"Bharat Singh",
+    Dob:'20-jan-2004'
+}
+console.log(obj)          */
+
+function fun(){
+    console.log("Hello World")
+}
+console.log(fun())
+
+let fun2=function(){
+    console.log("Hello jiii")
+}
+console.log(fun2())
+
+
